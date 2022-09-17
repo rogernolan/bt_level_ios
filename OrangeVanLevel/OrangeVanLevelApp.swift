ce8b33aeb9bafda4dff7ca18c216bdab26f7e2d7
Rog <rog@hatbat.net>
2022-09-17 13:45:52 +0100

    Initial Commit

---
+++ OrangeVanLevel/OrangeVanLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrangeVanLevelApp.swift
+//  OrangeVanLevel
+//
+//  Created by Roger Nolan on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct OrangeVanLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
