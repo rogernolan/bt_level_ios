d08b40fd56296d49c63482ba0b72e0ded7d1b29d
Rog <rog@hatbat.net>
2022-11-08 14:20:39 +0000

    add empty watch app. Does not build yet.

---
+++ Van Level Watch App/Van_LevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Van_LevelApp.swift
+//  Van Level Watch App
+//
+//  Created by Roger Nolan on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Van_Level_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
